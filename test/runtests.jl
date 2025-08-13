c8b51a6507dd037a6846b2ecc93a000151a443a6
Rui Rojo <rui.rojo@proton.me>
2025-08-13 05:56:29 -0300

    Test fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,16 +9,15 @@ using OceanData, Test, Documenter
 
         @test salinity((-50°, -50°); date)(12m) ≈ 34.03125f0
         @test temperature((-55°, -50°), 555m; date) ≈ 2.6070576f0°C
-
+        @test_throws ArgumentError salinity((-50, -50); date)(12m) ≈ 34.0625
+        @test_throws ArgumentError temperature((-55, -50), 555m; date) ≈ 2.5774062°C
+        
         @testset "Bathymetry" begin
             @test bathymetry((-3°, -15°)) ≈ 3587.250002881934m
             @test ismissing(bathymetry((3°, 15°); nothrow=true))
         end
     end
     
-
-    @test_throws ArgumentError salinity((-50, -50); date)(12m) ≈ 34.0625
-    @test_throws ArgumentError temperature((-55, -50), 555m; date) ≈ 2.5774062°C
 
     @testset "Standard depths" begin
         @test issorted(diff(standard_depths))
