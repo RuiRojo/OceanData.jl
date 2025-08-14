b69b4af114055997c7157a6d6e2f061a64b1be85
Rui Rojo <rui.rojo@proton.me>
2025-08-13 21:25:15 -0300

    CI fixes

--- docs/make.jl
+++ docs/make.jl
@@ -3,26 +3,20 @@ using Documenter
 
 DocMeta.setdocmeta!(OceanData, :DocTestSetup, :(using OceanData); recursive=true)
 
-if get(ENV, "CI", "false") != "true"
-    doctest(OceanData)
-else
-    @info "Skipping doctests in CI environment"
-end
-
 
 makedocs(;
     modules=[OceanData],
     authors="Rui Rojo <rui.rojo@gmail.com> and contributors",
     repo="https://github.com/RuiRojo/OceanData.jl/blob/{commit}{path}#{line}",
     sitename="OceanData.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://ruirojo.github.io/OceanData.jl",
-        assets=String[],
-    ),
+    format=Documenter.HTML(),
     doctest = false,
-    checkdocs = :none,
     pages=[
         "Home" => "index.md",
     ],
 )
+
+deploydocs(
+    repo = "github.com/RuiRojo/OceanData.jl.git",
+    devbranch = "main",
+   )
